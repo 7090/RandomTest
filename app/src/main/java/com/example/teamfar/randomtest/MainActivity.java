4d9a2d96b7ecdc40afc2d6e3538a25f0f61dd18c
7090 <teamfar7090@gmail.com>
2016-12-19 16:55:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/teamfar/randomtest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.teamfar.randomtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testupload (){
+
+        int i = 1+1;
+    }
+}
